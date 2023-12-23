48a788c772474aa6eadbd700731815d4563b83f4
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2023-12-23 10:43:59 +0530

    basic signup view form and related tests

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 
+from accounts import views as accounts_views
 from boards import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup/', accounts_views.signup, name='signup'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     # path('boards/<int:pk>', views.new_topic, name='new_topic'),
