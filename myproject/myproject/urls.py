0e735fec9f162265a374d3339e82b122635b6e03
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2023-12-30 15:01:57 +0530

    adding update post view using CBV and testing it

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -49,5 +49,6 @@ urlpatterns = [
     path('boards/<pk>/', views.board_topics, name='board_topics'),
     path('boards/<pk>/topics/<topic_pk>/', views.topic_posts, name='topic_posts'),
     path('boards/<pk>/topics/<topic_pk>/reply/', views.reply_topic, name='reply_topic'),
+    path('boards/<pk>/topics/<topic_pk>/posts/<post_pk>/edit/', views.PostUpdateView.as_view(), name='edit_post'),
     path('admin/', admin.site.urls),
 ]
