a56ccdc0fba74694c0829aa0d7288354152a4423
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2023-12-31 11:11:59 +0530

    adding markdown editor

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.text import Truncator
+from django.utils.html import mark_safe
+from markdown import markdown
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -37,3 +39,6 @@ class Post(models.Model):
     def __str__(self):
         truncated_message = Truncator(self.message)
         return truncated_message.chars(30)
+
+    def get_message_as_markdown(self):
+        return mark_safe(markdown(self.message, safe_mode='escape'))
